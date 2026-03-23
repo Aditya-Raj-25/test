4b7e60c7f563d22afcde7b94523a8cf1cbf9242c
Aditya-Raj-25 <adityarajsrivastava227@gmail.com>
2026-03-23 10:37:42 +0530

    Sync leetcode solution: zigzag conversion [Code]

---
+++ leetcode/zigzag-conversion/solution.py
@@ -0,0 +1,12 @@
+        for char in s:
+            rows[idx].append(char)
+            if idx == 0:
+                d = 1
+            elif idx == numRows - 1:
+                d = -1
+            idx += d
+
+        for i in range(numRows):
+            rows[i] = ''.join(rows[i])
+
+        return ''.join(rows)   
